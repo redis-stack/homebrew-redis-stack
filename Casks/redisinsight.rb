65c86c3923f85789093882bbb418d3e70fc36d36
chayim <c@kirshen.com>
2022-02-27 12:27:24 +0000

    initial redisinsight cask

---
+++ Casks/redisinsight.rb
@@ -0,0 +1,11 @@
+cask "redisinsight" do
+  version "2.0.4-preview"
+  name "RedisInsight"
+  desc "Placeholder"
+  homepage "https://redis.com/redis-enterprise/redis-insight/"
+
+  url "https://s3.amazonaws.com/redisinsight.test/public/rs-ri-builds/redisinsight.app-darwin-x64.tar.gz"
+  sha256 "f82e6c210673a2439e42429342b9b35cb5bfdc8b00a88a45e0f4bc2b34b331e3"
+
+  app "RedisInsight-preview.app"
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# redis-stack homebrew
+
+## Installation
+
+Enable the repostiory:
+
+```
+brew tap redislabs/redis-stack
+```
+
+```
+brew install PACKAGE_NAME
+```
+
+Install a cask (i.e RedisInsight) from the repository:
+
+```
+brew install --cask redisinsight
+```
+
