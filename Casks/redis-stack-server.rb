66c021ff92df32f96caf03eca7cb7bf8269a0caf
ofekshenawa <ofek.shenawa@redis.com>
2023-10-25 10:23:41 +0300

    Redis Stack 7.2.0-v5

--- Casks/redis-stack-server.rb
+++ Casks/redis-stack-server.rb
@@ -2,7 +2,7 @@ cask "redis-stack-server" do
 
   homepage "https://redis.io"
   desc "Redis Stack Server install a Redis server with additional database capabilities"
-  version "7.2.0-v4"
+  version "7.2.0-v5"
 
   baseos = 'darwin'
   if RUBY_PLATFORM.index('darwin') != nil
