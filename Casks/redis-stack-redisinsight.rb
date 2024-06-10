193eaeceb56e80c4ece75554473ac594debe3b52
vladvildanov <divinez122@outlook.com>
2024-06-10 13:13:16 +0300

    Change app name to correct one

--- Casks/redis-stack-redisinsight.rb
+++ Casks/redis-stack-redisinsight.rb
@@ -17,7 +17,7 @@ cask "redis-stack-redisinsight" do
   end
 
   url "https://s3.amazonaws.com/redisinsight.download/public/releases/#{version}/redisstack/Redis-Insight-app-#{baseos}.#{platform}.tar.gz"
-  app "Redis-Insight.app"
+  app "Redis Insight.app"
 
   uninstall_preflight do
     if RUBY_PLATFORM.index('x86') != nil
@@ -37,7 +37,7 @@ cask "redis-stack-redisinsight" do
     dest = "#{basepath}/redisinsight"
     File.open(dest, 'w+') { |f|
       f.write("#!/bin/sh\n\n")
-      f.write("open /Applications/Redis-Insight.app\n")
+      f.write("open /Applications/Redis Insight.app\n")
     }
     FileUtils.chmod(0755, dest)
   end
