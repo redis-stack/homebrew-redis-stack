dc7d0ca1d12ee29687c077da74c4412af82c7f24
Patrick Linnane <patrick@linnane.io>
2024-12-03 09:58:39 -0800

    redis-stack: rewrite for modern Homebrew conventions
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/redis-stack.rb
+++ Casks/redis-stack.rb
@@ -1,15 +1,13 @@
 cask "redis-stack" do
-
-  homepage "https://redis.io"
-  name "Redis Stack"
-  desc "Redis server with additional database capabilities and the visualisation tool RedisInsight."
   version "7.4.0-v1"
-
-  # FIXME PLACEHOLDER
-  url "https://redismodules.s3.amazonaws.com/redis-stack/.donotremove"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  # FIXME: PLACEHOLDER
+  url "https://redismodules.s3.amazonaws.com/redis-stack/.donotremove"
+  name "Redis Stack"
+  desc "Redis server with additional database capabilities and the visualisation tool RedisInsight"
+  homepage "https://redis.io/"
 
   depends_on cask: "redis-stack-server"
   depends_on cask: "redis-stack-redisinsight"
-
 end
