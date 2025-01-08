51ec3138657e044687b2565110dd20a64674413a
vladvildanov <divinez122@outlook.com>
2025-01-08 11:31:05 +0200

    Updated SHA sum for insight

--- Casks/redis-stack-redisinsight.rb
+++ Casks/redis-stack-redisinsight.rb
@@ -2,8 +2,8 @@ cask "redis-stack-redisinsight" do
   arch arm: "arm64", intel: "x64"
 
   version "2.64.1"
-  sha256 arm:   "2da08a97de6d7349622401a78b6323bca1a1f1c29079b91e937e9eaeec56f628",
-         intel: "5adb0f72fe0f5751bfeb43783cf0655dc8645099685dc48d372d811b2946286f"
+  sha256 arm:   "3a51e4bba14ba1d0348c9566f9bc57414eece16c92852585ab656e6e60a4bb06",
+         intel: "f6ac62d3125b683b377f1588deef3c9e17d7758ce46e4cefb502f45f62f2ee98"
 
   url "https://s3.amazonaws.com/redisinsight.download/public/releases/#{version}/redisstack/Redis-Insight-app-darwin.#{arch}.tar.gz"
   name "RedisInsight"
