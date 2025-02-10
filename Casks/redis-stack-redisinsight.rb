1c3a1965f8b763f0c3b52748c8492cd36daa4c2d
vladvildanov <vladyslav.vildanov@redis.com>
2025-02-10 11:09:55 +0200

    7.4.0-v3

--- Casks/redis-stack-redisinsight.rb
+++ Casks/redis-stack-redisinsight.rb
@@ -1,7 +1,7 @@
 cask "redis-stack-redisinsight" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.64.1"
+  version "2.66.0"
   sha256 arm:   "3a51e4bba14ba1d0348c9566f9bc57414eece16c92852585ab656e6e60a4bb06",
          intel: "f6ac62d3125b683b377f1588deef3c9e17d7758ce46e4cefb502f45f62f2ee98"
 
