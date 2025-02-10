1df518ef8d6cef16116fbf74d3a700b55e45e086
Vladyslav Vildanov <117659936+vladvildanov@users.noreply.github.com>
2025-02-10 11:19:22 +0200

    7.4.0-v3 (#66)
    * 7.4.0-v3
    
    * Updated SHA256 sums for server
    
    * Updated SHA256 sums for insigth

--- Casks/redis-stack-redisinsight.rb
+++ Casks/redis-stack-redisinsight.rb
@@ -1,9 +1,9 @@
 cask "redis-stack-redisinsight" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.64.1"
-  sha256 arm:   "3a51e4bba14ba1d0348c9566f9bc57414eece16c92852585ab656e6e60a4bb06",
-         intel: "f6ac62d3125b683b377f1588deef3c9e17d7758ce46e4cefb502f45f62f2ee98"
+  version "2.66.0"
+  sha256 arm:   "8db50a12a64a5bbafc8e39077d192b7209d7df7d2b82635ee4cbdcd8f821dfbb",
+         intel: "3ccf129ef1179ddd13ff048907ea77a5e7d6f9c1c2dfda496e6cd596b6d04310"
 
   url "https://s3.amazonaws.com/redisinsight.download/public/releases/#{version}/redisstack/Redis-Insight-app-darwin.#{arch}.tar.gz"
   name "RedisInsight"
